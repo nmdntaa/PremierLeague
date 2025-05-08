8dd8eb9730fd4a60d9e796db1fbd0885f18360ae
nmdntaa <144686774+nmdntaa@users.noreply.github.com>
2025-05-08 15:21:54 +0700

    feat :[NM] laliga

--- app/src/main/java/com/example/premierleague/laLiga.java
+++ app/src/main/java/com/example/premierleague/laLiga.java
@@ -23,7 +23,38 @@ public class laLiga extends AppCompatActivity {
     TeamAdapter adapter;
     List<Team> teams = new ArrayList<>();
 
-    //@Override
-    //protected void onCreate(Call<TeamResponse>);
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_la_liga);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        ApiService apiService = ApiClient.getRetrofitInstance().create(ApiService.class);
+        apiService.getAllTeams("Spanish La Liga").enqueue(new Callback<TeamResponse>() {
+            @Override
+            public void onResponse(Call<TeamResponse> call, Response<TeamResponse> response) {
+                progressBar.setVisibility(View.GONE);
+                if (response.isSuccessful() && response.body() !=null){
+                    teams = response.body().getTeams();
+                    adapter = new TeamAdapter(laLiga.this, teams);
+                    recyclerView.setAdapter(adapter);
+                } else {
+                    Toast.makeText(laLiga.this, "Data kosong atau error", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TeamResponse> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(laLiga.this, "Gagal mengambil data", Toast.LENGTH_SHORT);
+                t.printStackTrace();
+            }
+        });
+    }
 }
 
